40a030b8e3aca219366a86ddd6fdf887608770d6
tankicagoneva <tankicaspasev@gmail.com>
2022-12-16 19:44:37 +0000

    remove env variables

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  spaceId: 'wn4okoywryaq',
-  accessToken: 'HQ6rJi2WU25mhpcdy_NSqmH_UkS68gbTJF6ja23Hiu4',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  spaceId: 'wn4okoywryaq',
-  accessToken: 'HQ6rJi2WU25mhpcdy_NSqmH_UkS68gbTJF6ja23Hiu4',
-};
