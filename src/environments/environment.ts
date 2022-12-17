19547cb781e9ec7cd5bb2cdec9b157f15ee94916
tankicagoneva <tankicaspasev@gmail.com>
2022-12-17 20:58:16 +0000

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  SPACE_ID: process.env['SPACE_ID'],
-  ACCESS_TOKEN: process.env['ACCESS_TOKEN'],
+  SPACE_ID: process.env.SPACE_ID,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
 };
