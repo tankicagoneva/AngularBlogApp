17642a43252aa1a801218d9a682bad62d0acdab6
tankicagoneva <tankicaspasev@gmail.com>
2022-12-17 18:06:09 +0000

    remove env

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { env } from 'src/environments/environment.localhost_default';
-
-export const environment = {
-  production: true,
-  spaceId: env.spaceId,
-  accessToken: env.accessToken,
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-import { env } from 'src/environments/environment.localhost_default';
-
-export const environment = {
-  production: false,
-  spaceId: env.spaceId,
-  accessToken: env.accessToken,
-};
